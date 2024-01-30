90a383b8d3fb3d33a07f1eceb37ed0d8903fb669
belikov_artem <dev.dreamvention@gmail.com>
2024-01-30 19:19:28 +0200

    INSTALL 3.x.x

---
+++ admin/language/fr-fr/extension/payment/worldline.php
@@ -0,0 +1,173 @@
+<?php
+// Heading
+$_['heading_title']		 						= 'Worldline';
+
+// Text
+$_['text_worldline']		 					= '<img src="view/image/payment/worldline.svg" alt="Worldline" title="Worldline" />';
+$_['text_extensions']   						= 'Extensions';
+$_['text_edit']        							= 'Modifier Worldline';
+$_['text_version']								= 'Nouvelle version Worldline disponible. Vous pouvez le télécharger <a href="%s" target="_blank" class="alert-link">ici</a> !';
+$_['text_tab_account']				 			= 'Paramètres du compte';
+$_['text_tab_advanced']				 			= 'Réglages avancés';
+$_['text_tab_order_status']						= 'Statut de la commande';
+$_['text_tab_transaction']						= 'Transactions';
+$_['text_tab_suggest']							= 'Contactez-nous';
+$_['text_live']   								= 'Vivre';
+$_['text_test']   								= 'Test';
+$_['text_api_credential_alert']					= 'Pour récupérer la API Key et le API Secret dans votre PSPID, suivez ces étapes :<br />1) Connectez-vous au Back Office<br />2) Allez dans Configuration > Technical information > Ingenico Direct Settings > Direct API Key<br / >3) Si vous n\'avez encore rien configuré, l\'écran affiche "No API credentials found". Pour créer à la fois la API Key et le API Secret, cliquez sur "GENERATE"';
+$_['text_webhook_credential_alert']				= 'Pour récupérer les identifiants des webhooks, suivez ces étapes :<br />1) Connectez-vous au Back Office<br />2) Allez dans Configuration > Technical information > Ingenico Direct settings > Webhooks Configuration<br />3) Cliquez sur "GENERATE WEBHOOKS API KEY"<br />4) Copiez et collez immédiatement le WebhooksKeySecret<br />5) Dans "Endpoints URLs", collez Webhook URL de votre boutique<br />5) Cliquez sur "SAVE" pour confirmer vos paramètres';
+$_['text_pre_authorization']   					= 'Pré Autorisation';
+$_['text_final_authorization']   				= 'Finale Autorisation';
+$_['text_sale']   								= 'Vente';
+$_['text_no_preference']						= 'Pas de préférence';
+$_['text_no_challenge_requested']				= 'Aucun défi demandé';
+$_['text_challenge_requested']					= 'Défi demandé';
+$_['text_challenge_required']					= 'Défi requis';
+$_['text_exemption_none']						= 'Aucun signalement d’exemption';
+$_['text_exemption_transaction_risk_analysis']	= 'La transaction présente un faible risque';
+$_['text_exemption_low_value']					= 'La valeur de la transaction est inférieure à 30 EUR';
+$_['text_exemption_whitelist']					= 'Mis sur liste blanche par le client';
+$_['text_created_status']						= 'Statut créé';
+$_['text_cancelled_status']						= 'Statut annulé';
+$_['text_rejected_status']						= 'Statut rejeté';
+$_['text_pending_status']						= 'Statut en attente';
+$_['text_captured_status']						= 'Statut capturé';
+$_['text_refunded_status']						= 'Statut remboursé';
+$_['text_created']								= 'Créé';
+$_['text_cancelled']							= 'Annulé';
+$_['text_rejected']								= 'Rejeté';
+$_['text_rejected_capture']						= 'Capture rejetée';
+$_['text_pending_capture']						= 'En attente de capture';
+$_['text_captured']								= 'Capturé';
+$_['text_refunded']								= 'Remboursé';
+$_['text_authorization_requested']				= 'Autorisation demandée';
+$_['text_capture_requested']					= 'Capture demandée';
+$_['text_refund_requested']						= 'Remboursement demandé';
+$_['text_suggest_subject']						= 'Suggérer une nouvelle fonctionnalité';
+$_['text_suggest_version']						= 'OpenCart %s. Worldline %s.';
+$_['text_transaction_created']					= 'La transaction a été créée et est toujours en attente.';
+$_['text_transaction_cancelled']				= 'La transaction/autorisation a été annulée soit par votre client, soit par vous-même.';
+$_['text_transaction_rejected']					= 'La demande d\'autorisation/remboursement a été rejetée par l\'acquéreur.';
+$_['text_transaction_rejected_capture']			= 'La demande de capture a été rejetée par l\'acquéreur.';
+$_['text_transaction_pending_capture']			= 'La demande d\'autorisation a abouti, mais vous devez encore la capturer pour recevoir les fonds.';
+$_['text_transaction_captured']					= 'La demande de capture a réussi. Vous pouvez vous attendre à recevoir les fonds pour cette transaction de votre acquéreur.';
+$_['text_transaction_refunded']					= 'La demande de remboursement a abouti. Votre client peut s\'attendre à recevoir les fonds pour cette transaction de la part de ses émetteurs.';
+$_['text_transaction_authorization_requested']	= 'Worldline traite la demande d\'autorisation et attend le résultat.';
+$_['text_transaction_refund_requested']			= 'Worldline traite la demande de remboursement et attend le résultat.';
+$_['text_transaction_capture_requested']		= 'Worldline traite la demande de capture et attend le résultat.';
+$_['text_payment_information']					= 'Informations de paiement';
+$_['text_transaction_id']						= 'ID de transaction';
+$_['text_transaction_status']					= 'Statut de la transaction';
+$_['text_transaction_description']				= 'Description de la transaction';
+$_['text_payment_product']						= 'Mode de paiement';
+$_['text_total']								= 'Total';
+$_['text_amount']								= 'Le montant payé';
+$_['text_currency_code']						= 'Devise';
+$_['text_date']									= 'Date';
+$_['text_environment']							= 'Environnement';
+$_['text_card_bin']								= 'BIN de la carte';
+$_['text_card_number']							= 'Numéro de carte';
+$_['text_transaction_action']					= 'Action';
+$_['text_fraud_information']					= 'Informations sur la fraude';
+$_['text_fraud_result']							= 'Résultat de la fraude';
+$_['text_liability']							= 'Responsabilité pour 3DS';
+$_['text_exemption']							= 'Exemption';
+$_['text_authentication_status']				= 'Statut d\'authentification';
+
+// Entry
+$_['entry_environment']							= 'Environnement';
+$_['entry_merchant_id']							= 'Merchant ID (PSPID)';
+$_['entry_api_key']								= 'API Key';
+$_['entry_api_secret']							= 'API Secret';
+$_['entry_api_endpoint']						= 'API Endpoint';
+$_['entry_webhook_key']							= 'Webhook Key';
+$_['entry_webhook_secret']						= 'Webhook Secret';
+$_['entry_merchant_id_test']					= 'Test Merchant ID (PSPID)';
+$_['entry_api_key_test']						= 'Test API Key';
+$_['entry_api_secret_test']						= 'Test API Secret';
+$_['entry_api_endpoint_test']					= 'Test API Endpoint';
+$_['entry_webhook_key_test']					= 'Test Webhook Key';
+$_['entry_webhook_secret_test']					= 'Test Webhook Secret';
+$_['entry_webhook_url']   						= 'Webhook URL';
+$_['entry_cron_url']	  						= 'Cron URL';
+$_['entry_status']       						= 'Statut';
+$_['entry_title']								= 'Titre du paiement';
+$_['entry_button_title']						= 'Titre du bouton de paiement';
+$_['entry_authorization_mode']					= 'Mode d\'autorisation';
+$_['entry_group_cards']							= 'Cartes de groupe';
+$_['entry_forced_tokenization']					= 'Tokenisation forcée';
+$_['entry_tds_status']							= 'Statut 3DS';
+$_['entry_challenge_indicator']					= 'Indicateur de défi 3DS';
+$_['entry_exemption_request']					= 'Demande d\'exemption 3DS';
+$_['entry_template']							= 'Nom du fichier modèle';
+$_['entry_debug']				 				= 'Journalisation du débogage';
+$_['entry_total']		 						= 'Total';
+$_['entry_geo_zone']     						= 'Zone géographique';
+$_['entry_sort_order']   						= 'Classement';
+$_['entry_order_id']							= 'Numéro de commande';
+$_['entry_transaction_id']						= 'ID de transaction';
+$_['entry_transaction_status']					= 'Statut de la transaction';
+$_['entry_payment_product']						= 'Mode de paiement';
+$_['entry_amount']								= 'Le montant payé';
+$_['entry_currency']							= 'Devise';
+$_['entry_date_from']							= 'Dater de';
+$_['entry_date_to']								= 'Date de';
+$_['entry_company_name']   						= 'Nom de l\'entreprise';
+$_['entry_message']   							= 'Message';
+
+// Help
+$_['help_webhook_url']		  					= 'Configurez des webhooks dans le Back Office Worldline pour appeler cette URL.';
+$_['help_cron_url']		  						= 'Définissez un cron pour appeler cette URL.';
+$_['help_template']								= 'Si vous utilisez un modèle personnalisé, veuillez saisir le nom ici. Si vide, la page de paiement standard sera affichée. L’apparence et la convivialité de la page de paiement peuvent être personnalisées sur le Back Office Worldline.';
+$_['help_total']         						= 'Le montant total que la commande doit atteindre avant que ce mode de paiement ne devienne actif.';
+$_['help_company_name']          				= 'Le nom de votre société.';
+$_['help_message']          					= 'Veuillez expliquer comment notre plugin de paiement peut être encore amélioré.';
+
+// Column
+$_['column_order_id']							= 'Numéro de commande';
+$_['column_transaction_id']						= 'ID de transaction';
+$_['column_transaction_status']					= 'Statut de la transaction';
+$_['column_payment_product']					= 'Mode de paiement';
+$_['column_total']								= 'Total';
+$_['column_amount']								= 'Le montant payé';
+$_['column_currency_code']						= 'Devise';
+$_['column_date']								= 'Date';
+$_['column_environment']						= 'Environnement';
+$_['column_action']								= 'Action';
+
+// Button
+$_['button_save']  								= 'Sauvegarder';
+$_['button_sign_up']							= 'Créer un compte / S\'inscrire';
+$_['button_contact_us']							= 'Contactez-nous';
+$_['button_reset_api_endpoint']					= 'Réinitialiser API Endpoint';
+$_['button_copy_url']							= 'Copier l\'URL dans le presse-papiers';
+$_['button_filter']								= 'Filtrer';
+$_['button_send_suggest']						= 'Envoyer';
+$_['button_capture']							= 'Capturer';
+$_['button_cancel']								= 'Annuler';
+$_['button_refund']								= 'Remboursement';
+$_['button_title']								= 'Payer avec Worldline';
+
+// Success
+$_['success_save']								= 'Succès: Vous avez modifié Worldline!';
+$_['success_send_suggest']						= 'Succès: Vos informations ont été envoyées avec succès à Worldline!';
+$_['success_capture']							= 'La transaction a été capturée.';
+$_['success_cancel']							= 'La transaction/autorisation a été annulée.';
+$_['success_refund']							= 'La transaction a été remboursée.';
+
+// Error
+$_['error_warning']          					= 'Attention: Veuillez vérifier attentivement le formulaire pour détecter les erreurs!';
+$_['error_permission'] 							= 'Attention: Vous n\'avez pas la permission de modifier le paiement Worldline!';
+$_['error_merchant_id']          				= 'Merchant ID est incorrect!';
+$_['error_api_key']          					= 'API Key est incorrect!';
+$_['error_api_secret']          				= 'API Secret est incorrect!';
+$_['error_webhook_key']          				= 'Webhook Key est incorrect!';
+$_['error_webhook_secret']          			= 'Webhook Secret est incorrect!';
+$_['error_merchant_id_test']          			= 'Test Merchant ID est incorrect!';
+$_['error_api_key_test']          				= 'Test API Key est incorrect!';
+$_['error_api_secret_test']          			= 'Test API Secret est incorrect!';
+$_['error_webhook_key_test']          			= 'Test Webhook Key est incorrect!';
+$_['error_webhook_secret_test']          		= 'Test Webhook Secret est incorrect!';
+$_['error_template']          					= 'Le nom du fichier modèle est incorrect! Il doit avoir l\'extension .htm, .html ou .dhtml!';
+$_['error_company_name']          				= 'Le nom de l’entreprise doit comporter entre 3 et 32 caractères!';
+$_['error_message']          					= 'Le message doit comporter entre 20 et 1000 caractères!';
---
+++ admin/model/extension/payment/worldline.php
@@ -0,0 +1,374 @@
+<?php
+class ModelExtensionPaymentWorldline extends Model {
+					
+	public function addWorldlineCustomerToken($data) {
+		$sql = "INSERT INTO `" . DB_PREFIX . "worldline_customer_token` SET";
+
+		$implode = array();
+			
+		if (!empty($data['customer_id'])) {
+			$implode[] .= "`customer_id` = '" . (int)$data['customer_id'] . "'";
+		}
+		
+		if (!empty($data['payment_type'])) {
+			$implode[] .= "`payment_type` = '" . $this->db->escape($data['payment_type']) . "'";
+		}
+		
+		if (!empty($data['token'])) {
+			$implode[] .= "`token` = '" . $this->db->escape($data['token']) . "'";
+		}
+				
+		if ($implode) {
+			$sql .= implode(", ", $implode);
+		}
+		
+		$this->db->query($sql);
+	}
+	
+	public function deleteWorldlineCustomerTokens($customer_id) {
+		$query = $this->db->query("DELETE FROM `" . DB_PREFIX . "worldline_customer` WHERE `customer_id` = '" . (int)$customer_id . "'");
+	}
+	
+	public function setWorldlineCustomerMainToken($customer_id, $payment_type, $token) {
+		$this->db->query("UPDATE `" . DB_PREFIX . "worldline_customer_token` SET `main_token_status` = '0' WHERE `customer_id` = '" . (int)$customer_id . "' AND `payment_type` = '" . $this->db->escape($payment_type) . "'");
+		$this->db->query("UPDATE `" . DB_PREFIX . "worldline_customer_token` SET `main_token_status` = '1' WHERE `customer_id` = '" . (int)$customer_id . "' AND `payment_type` = '" . $this->db->escape($payment_type) . "' AND `token` = '" . $this->db->escape($token) . "'");
+	}
+	
+	public function getWorldlineCustomerToken($customer_id, $payment_type, $token) {
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "worldline_customer_token` WHERE `customer_id` = '" . (int)$customer_id . "' AND `payment_type` = '" . $this->db->escape($payment_type) . "' AND `token` = '" . $this->db->escape($token) . "'");
+
+		if ($query->num_rows) {
+			return $query->row;
+		} else {
+			return array();
+		}
+	}
+	
+	public function getWorldlineCustomerTokens($customer_id) {
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "worldline_customer_token` WHERE `customer_id` = '" . (int)$customer_id . "'");
+
+		if ($query->num_rows) {
+			return $query->rows;
+		} else {
+			return array();
+		}
+	}
+
+	public function editWorldlineOrder($data) {
+		$sql = "UPDATE `" . DB_PREFIX . "worldline_order` SET";
+
+		$implode = array();
+		
+		if (!empty($data['transaction_id'])) {
+			$implode[] .= "`transaction_id` = '" . $this->db->escape($data['transaction_id']) . "'";
+		}
+					
+		if (!empty($data['transaction_status'])) {
+			$implode[] .= "`transaction_status` = '" . $this->db->escape($data['transaction_status']) . "'";
+		}
+		
+		if (!empty($data['payment_product'])) {
+			$implode[] .= "`payment_product` = '" . $this->db->escape($data['payment_product']) . "'";
+		}
+		
+		if (!empty($data['payment_type'])) {
+			$implode[] .= "`payment_type` = '" . $this->db->escape($data['payment_type']) . "'";
+		}
+		
+		if (!empty($data['token'])) {
+			$implode[] .= "`token` = '" . $this->db->escape($data['token']) . "'";
+		}
+		
+		if (!empty($data['total'])) {
+			$implode[] .= "`total` = '" . (float)$data['total'] . "'";
+		}
+		
+		if (!empty($data['amount'])) {
+			$implode[] .= "`amount` = '" . (float)$data['amount'] . "'";
+		}
+		
+		if (!empty($data['currency_code'])) {
+			$implode[] .= "`currency_code` = '" . $this->db->escape($data['currency_code']) . "'";
+		}
+		
+		if (!empty($data['country_code'])) {
+			$implode[] .= "`country_code` = '" . $this->db->escape($data['country_code']) . "'";
+		}
+		
+		if (!empty($data['environment'])) {
+			$implode[] .= "`environment` = '" . $this->db->escape($data['environment']) . "'";
+		}
+		
+		$implode[] .= "`date` = COALESCE(`date`, NOW())";
+		
+		if ($implode) {
+			$sql .= implode(", ", $implode);
+		}
+
+		$sql .= " WHERE `order_id` = '" . (int)$data['order_id'] . "'";
+		
+		$this->db->query($sql);
+	}
+		
+	public function deleteWorldlineOrder($order_id) {
+		$query = $this->db->query("DELETE FROM `" . DB_PREFIX . "worldline_order` WHERE `order_id` = '" . (int)$order_id . "'");
+	}
+	
+	public function getWorldlineOrder($order_id) {
+		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "worldline_order` WHERE `order_id` = '" . (int)$order_id . "'");
+		
+		if ($query->num_rows) {
+			return $query->row;
+		} else {
+			return array();
+		}
+	}
+	
+	public function getWorldlineOrders($data = array()) {
+		$sql = "SELECT wo.order_id, wo.transaction_id, wo.transaction_status, wo.payment_product, wo.payment_type, wo.token, wo.total, wo.amount, wo.currency_code, wo.date, wo.environment FROM `" . DB_PREFIX . "worldline_order` wo";
+
+		$implode = array();
+			
+		if (!empty($data['filter_order_id'])) {
+			$implode[] .= "wo.order_id = '" . (int)$data['filter_order_id'] . "'";
+		}
+		
+		if (!empty($data['filter_transaction_id'])) {
+			$implode[] .= "wo.transaction_id = '" . $this->db->escape($data['filter_transaction_id']) . "'";
+		}
+		
+		if (isset($data['filter_transaction_status'])) {
+			$implode[] .= "wo.transaction_status = '" . $this->db->escape($data['filter_transaction_status']) . "'";
+		}
+		
+		if (isset($data['filter_payment_product'])) {
+			$implode[] .= "wo.payment_product LIKE '%" . $this->db->escape($data['filter_payment_product']) . "%'";
+		}
+		
+		if (isset($data['filter_payment_type'])) {
+			$implode[] .= "wo.payment_type = '" . $this->db->escape($data['filter_payment_type']) . "'";
+		}
+		
+		if (isset($data['filter_token'])) {
+			$implode[] .= "wo.token = '" . $this->db->escape($data['filter_token']) . "'";
+		}
+		
+		if (!empty($data['filter_total'])) {
+			$implode[] = "wo.total = '" . (float)$data['filter_total'] . "'";
+		}
+		
+		if (!empty($data['filter_amount'])) {
+			$implode[] = "wo.amount = '" . (float)$data['filter_amount'] . "'";
+		}
+		
+		if (!empty($data['filter_currency_code'])) {
+			$implode[] = "wo.currency_code = '" . $this->db->escape($data['filter_currency_code']) . "'";
+		}
+				
+		if (!empty($data['filter_date_from'])) {
+			$implode[] .= "DATE(wo.date) >= DATE('" . $this->db->escape($data['filter_date_from']) . "')";
+		}
+		
+		if (!empty($data['filter_date_to'])) {
+			$implode[] .= "DATE(wo.date) <= DATE('" . $this->db->escape($data['filter_date_to']) . "')";
+		}
+		
+		if (!empty($data['filter_environment'])) {
+			$implode[] = "wo.environment = '" . $this->db->escape($data['filter_environment']) . "'";
+		}
+		
+		if ($implode) {
+			$sql .= " WHERE " . implode(" AND ", $implode);
+		}
+		
+		
+		$sort_data = array(
+			'wo.order_id',
+			'wo.transaction_id',
+			'wo.transaction_status',
+			'wo.payment_product',
+			'wo.payment_type',
+			'wo.token',
+			'wo.total',
+			'wo.amount',
+			'wo.currency_code',
+			'wo.date',
+			'wo.environment'
+		);
+
+		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+			$sql .= " ORDER BY " . $data['sort'];
+		} else {
+			$sql .= " ORDER BY wo.order_id";
+		}
+
+		if (isset($data['order']) && ($data['order'] == 'DESC')) {
+			$sql .= " DESC";
+		} else {
+			$sql .= " ASC";
+		}
+
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}
+
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}
+
+		$query = $this->db->query($sql);
+
+		return $query->rows;
+	}
+	
+	public function getTotalWorldlineOrders($data = array()) {
+		$sql = "SELECT COUNT(DISTINCT order_id) AS total FROM `" . DB_PREFIX . "worldline_order` wo";
+
+		$implode = array();
+			
+		if (!empty($data['filter_order_id'])) {
+			$implode[] .= "wo.order_id = '" . (int)$data['filter_order_id'] . "'";
+		}
+		
+		if (!empty($data['filter_transaction_id'])) {
+			$implode[] .= "wo.transaction_id = '" . $this->db->escape($data['filter_transaction_id']) . "'";
+		}
+		
+		if (isset($data['filter_transaction_status'])) {
+			$implode[] .= "wo.transaction_status = '" . $this->db->escape($data['filter_transaction_status']) . "'";
+		}
+		
+		if (isset($data['filter_payment_product'])) {
+			$implode[] .= "wo.payment_product LIKE '%" . $this->db->escape($data['filter_payment_product']) . "%'";
+		}
+		
+		if (isset($data['filter_payment_type'])) {
+			$implode[] .= "wo.payment_type = '" . $this->db->escape($data['filter_payment_type']) . "'";
+		}
+		
+		if (isset($data['filter_token'])) {
+			$implode[] .= "wo.token = '" . $this->db->escape($data['filter_token']) . "'";
+		}
+		
+		if (!empty($data['filter_total'])) {
+			$implode[] = "wo.total = '" . (float)$data['filter_total'] . "'";
+		}
+		
+		if (!empty($data['filter_amount'])) {
+			$implode[] = "wo.amount = '" . (float)$data['filter_amount'] . "'";
+		}
+		
+		if (!empty($data['filter_currency_code'])) {
+			$implode[] = "wo.currency_code = '" . $this->db->escape($data['filter_currency_code']) . "'";
+		}
+				
+		if (!empty($data['filter_date_from'])) {
+			$implode[] .= "DATE(wo.date) > DATE('" . $this->db->escape($data['filter_date_from']) . "')";
+		}
+		
+		if (!empty($data['filter_date_to'])) {
+			$implode[] .= "DATE(wo.date) < DATE('" . $this->db->escape($data['filter_date_to']) . "')";
+		}
+		
+		if (!empty($data['filter_environment'])) {
+			$implode[] = "wo.environment = '" . $this->db->escape($data['filter_environment']) . "'";
+		}
+		
+		if ($implode) {
+			$sql .= " WHERE " . implode(" AND ", $implode);
+		}
+		
+		$query = $this->db->query($sql);
+								
+		return $query->row['total'];
+	}
+	
+	public function addOrderHistory($order_id, $order_status_id, $comment = '', $notify = false, $override = false) {
+		$this->db->query("UPDATE `" . DB_PREFIX . "order` SET order_status_id = '" . (int)$order_status_id . "', date_modified = NOW() WHERE order_id = '" . (int)$order_id . "'");
+		$this->db->query("INSERT INTO " . DB_PREFIX . "order_history SET order_id = '" . (int)$order_id . "', order_status_id = '" . (int)$order_status_id . "', notify = '" . (int)$notify . "', comment = '" . $this->db->escape($comment) . "', date_added = NOW()");
+	}
+	
+	public function checkVersion($opencart_version, $worldline_version) {
+		$curl = curl_init();
+			
+		curl_setopt($curl, CURLOPT_URL, 'https://www.opencart.com/index.php?route=api/promotion/worldline&opencart=' . $opencart_version . '&worldline=' . $worldline_version);
+		curl_setopt($curl, CURLOPT_HEADER, 0);
+		curl_setopt($curl, CURLOPT_HEADER, 0);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
+		curl_setopt($curl, CURLOPT_FORBID_REUSE, 1);
+		curl_setopt($curl, CURLOPT_FRESH_CONNECT, 1);
+							
+		$response = curl_exec($curl);
+			
+		curl_close($curl);
+			
+		$result = json_decode($response, true);
+		
+		if ($result) {
+			return $result;
+		} else {
+			return false;
+		}
+	}
+		
+	public function sendSuggest($data) {
+		$_config = new Config();
+		$_config->load('worldline');
+		
+		$config_setting = $_config->get('worldline_setting');
+		
+		$setting = array_replace_recursive((array)$config_setting, (array)$this->config->get('payment_worldline_setting'));
+		
+		$data['text_suggest_version'] = sprintf($this->language->get('text_suggest_version'), VERSION, $setting['version']);
+		
+		$mail = new Mail($this->config->get('config_mail_engine'));
+		$mail->parameter = $this->config->get('config_mail_parameter');
+		$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
+		$mail->smtp_username = $this->config->get('config_mail_smtp_username');
+		$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
+		$mail->smtp_port = $this->config->get('config_mail_smtp_port');
+		$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
+
+		$mail->setTo('dl-dl_shoppingcarts@worldline.com');
+		$mail->setFrom($this->config->get('config_email'));
+		$mail->setSender(html_entity_decode($data['payment_worldline_setting']['suggest']['company_name'], ENT_QUOTES, 'UTF-8'));
+		$mail->setSubject(html_entity_decode($this->language->get('text_suggest_subject'), ENT_QUOTES, 'UTF-8'));
+		$mail->setHtml($this->load->view('extension/payment/worldline/suggest_mail', $data));
+		$mail->send();
+	}
+
+	public function log($data, $title = '') {
+		$_config = new Config();
+		$_config->load('worldline');
+		
+		$config_setting = $_config->get('worldline_setting');
+		
+		$setting = array_replace_recursive((array)$config_setting, (array)$this->config->get('payment_worldline_setting'));
+		
+		if ($setting['advanced']['debug']) {
+			$log = new Log('worldline.log');
+			
+			if (is_string($data)) {
+				$log->write('Worldline debug (' . $title . '): ' . $data);
+			} else {
+				$log->write('Worldline debug (' . $title . '): ' . json_encode($data));
+			}
+		}
+	}
+	
+	public function install() {
+		$this->db->query("CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "worldline_customer_token` (`customer_id` INT(11) NOT NULL, `payment_type` VARCHAR(20) NOT NULL, `token` VARCHAR(50) NOT NULL, `main_token_status` TINYINT(1) NOT NULL, PRIMARY KEY (`customer_id`, `payment_type`, `token`), KEY `main_token_status` (`main_token_status`)) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci");
+		$this->db->query("CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "worldline_order` (`order_id` INT(11) NOT NULL, `transaction_id` VARCHAR(20) NOT NULL, `transaction_status` VARCHAR(20) NULL, `payment_product` VARCHAR(40) NULL, `payment_type` VARCHAR(20) NOT NULL, `token` VARCHAR(50), `total` DECIMAL(15,2) NULL, `amount` DECIMAL(15,2) NULL, `currency_code` VARCHAR(3) NULL, `country_code` VARCHAR(2) NULL, `environment` VARCHAR(20) NULL, `date` DATETIME NULL, PRIMARY KEY (`order_id`), KEY `transaction_id` (`transaction_id`)) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci");
+	}
+	
+	public function uninstall() {
+		$this->db->query("DROP TABLE IF EXISTS `" . DB_PREFIX . "worldline_customer_token`");
+		$this->db->query("DROP TABLE IF EXISTS `" . DB_PREFIX . "worldline_order`");
+	}
+}
