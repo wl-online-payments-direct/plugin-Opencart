f9282fa01ff6d4d4cf04cc864db13c990cb829c6
belikov_artem <dev.dreamvention@gmail.com>
2024-07-13 00:46:43 +0300

    VERSION 1.0.3

--- admin/model/payment/worldline.php
+++ admin/model/payment/worldline.php
@@ -287,11 +287,6 @@ class ModelPaymentWorldline extends Model {
 		return $query->row['total'];
 	}
 	
-	public function addOrderHistory($order_id, $order_status_id, $comment = '', $notify = false, $override = false) {
-		$this->db->query("UPDATE `" . DB_PREFIX . "order` SET order_status_id = '" . (int)$order_status_id . "', date_modified = NOW() WHERE order_id = '" . (int)$order_id . "'");
-		$this->db->query("INSERT INTO " . DB_PREFIX . "order_history SET order_id = '" . (int)$order_id . "', order_status_id = '" . (int)$order_status_id . "', notify = '" . (int)$notify . "', comment = '" . $this->db->escape($comment) . "', date_added = NOW()");
-	}
-	
 	public function checkVersion($opencart_version, $worldline_version) {
 		$curl = curl_init();
 			
